677782872eb51e112148cd82cdef6b3a43f4af70
Kovarthanan-murugan <kovarthan.ece@gmail.com>
2023-12-04 07:17:08 -0400

    added image

--- Frontend/src/components/atoms/Dropdown/DropdownProfil/index.jsx
+++ Frontend/src/components/atoms/Dropdown/DropdownProfil/index.jsx
@@ -114,7 +114,7 @@ const DropdownProfil = () => {
             <img
               className='h-full w-full object-cover'
               // src={`${api}/images/${dataPegawai.photo}`}
-              src="../../.../../../../assets/images/bannerImg.png"
+              src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
               
               alt='Profil'
             />
